794b65e7b1b841c66cd9bbd3cc375daf067ee217
Maria-Carolina-Rinaldi <dr.rinaldimaria@gmail.com>
2024-10-02 17:55:39 +0200

    homepage design setup

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,6 +12,7 @@ class HomeScreen extends StatefulWidget {
   @override
   HomeScreenState createState() => HomeScreenState();
 }
+
 class HomeScreenState extends State<HomeScreen> {
   // Indice della pagina selezionata
   int _selectedIndex = 0;
@@ -25,80 +26,120 @@ class HomeScreenState extends State<HomeScreen> {
     role: '',  // Aggiungi un ruolo utente o admin
   );
 
-late final List<Widget> _pages;
+  late final List<Widget> _pages;
 
-@override
-void initState() {
-  super.initState();
-  _pages = [
-    Center(
+  @override
+  void initState() {
+    super.initState();
+    _pages = [
+      Center(
         child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('assets/images/intelligearth_logo.png', height: 50),
-            Text('IntelligEarth App', style: TextStyle(fontSize: 24)),
-            Text('Bentornat*!', style: TextStyle(fontSize: 24)),
-            Text('we are loading...', style: TextStyle(fontSize: 18)),
-        ],
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+
+            ],
+          ),
+        ),
       ),
-    ),
-    ),
-    PhotoPage(),    // Usa il widget della schermata mappa
-    TechsupportScreen(),    // Usa il widget della schermata report
-    UserPage(user: currentUser), 
-  ];
-}
+      PhotoPage(),    // Usa il widget della schermata mappa
+      TechsupportScreen(),    // Usa il widget della schermata report
+      UserPage(user: currentUser), 
+    ];
+  }
 
   // Funzione per aggiornare la selezione dell'indice
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
-    // Navigazione alla pagina dell'account
-  if (index == 3) { // Indice della pagina Account
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => UserPage(user: currentUser),
-      ),
-    );
   }
-}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('IntelligEarth'),
+        title: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: GestureDetector(
+                  onTap: () {
+                    // Logica per aprire il menu hamburger
+                  },
+                  child: Image.asset(
+                    'assets/icons/menu-burger.png',
+                    height: 24,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Center(
+                child: Text(
+                  'Homepage',
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.asset('assets/images/intelligearth_logo.png', height: 18),
+                    const SizedBox(width: 2),
+                    const Text('IntelligEarth', style: TextStyle(fontSize: 14)),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
       body: _pages[_selectedIndex],      
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
+        type: BottomNavigationBarType.fixed,
+        items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+            icon: Image.asset(
+            _selectedIndex == 0 ? 'assets/icons/home_selected.png' : 'assets/icons/home.png',
+            height: 24,
+          ),
+          label: '',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.map),
-            label: 'Mappa',
+            icon: Image.asset(
+              _selectedIndex == 1 ? 'assets/icons/storico_luogo_selected.png' : 'assets/icons/storico_luogo.png',
+              height: 24,
+            ),
+            label: '',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.report),
-            label: 'Report',
+            icon: Image.asset(
+              _selectedIndex == 2 ? 'assets/icons/supporto_tecnico_selected.png' : 'assets/icons/supporto_tecnico.png',
+              height: 24,
+            ),
+            label: '',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: 'Account',
+            icon: Image.asset(
+              _selectedIndex == 3 ? 'assets/icons/user_selected.png' : 'assets/icons/user.png',
+              height: 24,
+            ),
+            label: '',
           ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.green,
-        unselectedItemColor: Colors.blue,
         onTap: _onItemTapped,
       ),
     );
   }
 }
- 
